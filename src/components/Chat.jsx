9207d848fe37975de10ba0da527bb9f872856cec
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-03-27 15:50:30 +0530

    chat section navbar created

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,8 +2,26 @@ import React from 'react'
 
 const Chat = () => {
   return (
-    <div className='chat'>Chat</div>
-  )
+    <div className="chat">
+      <div className="chatInfo">
+        <span>Dadhi</span>
+        <div className="chatIcon">
+          <img
+            src="https://cdn-icons-png.flaticon.com/32/8353/8353783.png"
+            alt="Camera"
+          />
+          <img
+            src="https://cdn-icons-png.flaticon.com/32/3683/3683218.png"
+            alt="Add friend"
+          />
+          <img
+            src="https://cdn-icons-png.flaticon.com/32/512/512142.png"
+            alt="More"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Chat
