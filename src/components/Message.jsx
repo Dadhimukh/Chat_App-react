becbc802d5d1b986785da2314020483ddcb25901
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-03-27 21:50:18 +0530

    Message section compleated

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,16 @@
 
    const Message = () => {
    return (
-      <div className='message'>Message</div>
+      <div className='message owner'>
+         <div className="messageInfo">
+            <img src="https://images.pexels.com/photos/4759765/pexels-photo-4759765.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+            <span>just now</span>
+         </div>
+         <div className="messageContent">
+            <p>Hello</p>
+            <img src="https://images.pexels.com/photos/4759765/pexels-photo-4759765.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+         </div>
+      </div>
    )
    }
 
