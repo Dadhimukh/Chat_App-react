1f1f2449f7da2e0f1dc622fabcc51e2e10b41ee4
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-03-28 18:45:26 +0530

    Added authentacion for singUp

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from "./pages/Home";
 const App = () => {
   return (
     <div>
-      <Home />
+      <Register />
     </div>
   );
 };
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,13 +1,35 @@
    import React from "react";
+   import { createUserWithEmailAndPassword } from "firebase/auth";
+   import { auth } from "../firebase";
+
    import "../styles/style.scss";
 
    const Register = () => {
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(e.target[0].value);
+      const displayName = e.target[0].value;
+      const email = e.target[1].value;
+      const password = e.target[2].value;
+      const file = e.target[3].files[0];
+
+      createUserWithEmailAndPassword(auth, email, password)
+         .then((userCredential) => {
+         // Signed in
+         const user = userCredential.user;
+         console.log(user, "not found");
+         // ...
+         })
+         .catch((error) => {
+         // ..
+         });
+   };
    return (
       <div className="formContainer">
          <div className="formWrapper">
          <span className="logo">Chat App</span>
          <span className="title">Register</span>
-         <form action="">
+         <form onSubmit={handleSubmit}>
             <input type="text" placeholder="Enter your Name" />
             <input type="email" placeholder="Enter your Email" />
             <input type="password" placeholder="Password" />
