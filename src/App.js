5a3ca483f3c7cb58c5aba9bbded9e2fa1bccce11
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-03-28 22:17:58 +0530

    creating routes for Home, Login and Register page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
@@ -6,7 +7,15 @@ import Home from "./pages/Home";
 const App = () => {
   return (
     <div>
-      <Register />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />{" "}
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
