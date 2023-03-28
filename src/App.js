01f6f3905ff217af74f69f715025aae356106f20
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-03-28 22:54:39 +0530

    Showing the details of current user by using AuthContext

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthContext } from "./context/AuthContext";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 
 const App = () => {
+  const { currentUser } = useContext(AuthContext);
+  console.log(currentUser);
   return (
     <div>
       <BrowserRouter>
