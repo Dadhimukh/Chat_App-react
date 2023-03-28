03939eb7a9ee5f1aac97088b07d045634b0ad38b
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-03-28 22:45:18 +0530

    wrapping our App component into AuthContextProvider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { AuthContextprovider } from "./context/AuthContext";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <AuthContextprovider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </AuthContextprovider>
 );
