2cdead28aa90377d33f4b2ab66cc14309f243b3d
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-03-30 14:01:41 +0530

    created ProctedRoute and cover Home component to ProtectedROute,ProtectedROute for checking current user or not

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthContext } from "./context/AuthContext";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
@@ -8,14 +8,28 @@ import Home from "./pages/Home";
 const App = () => {
   // showing the detail of current user by using AuthContext
   const { currentUser } = useContext(AuthContext);
-  console.log(currentUser);
+  // console.log(currentUser);
+
+  // checking the current user and Navigating the pages
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+  };
 
   return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route path="/">
-            <Route index element={<Home />} />{" "}
+            <Route
+              index
+              element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              }
+            />{" "}
             <Route path="login" element={<Login />} />
             <Route path="register" element={<Register />} />
           </Route>
