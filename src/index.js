5943a3c03bdf7274665233cd3b06f8b3e33f0658
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-03-30 19:46:45 +0530

    used ChatContextProvider in index.js file

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { AuthContextProvider } from "./context/AuthContext";
 import App from "./App";
+import { ChatContextProvider } from "./context/ChatContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AuthContextProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <ChatContextProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ChatContextProvider>
   </AuthContextProvider>
 );
