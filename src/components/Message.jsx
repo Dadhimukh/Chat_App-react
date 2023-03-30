5bf4e444c2963109c9cbe5e7d7609ee135493186
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-03-30 19:39:37 +0530

    showing data dinamically from messages collection ffrom firebase

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,18 +1,39 @@
-   import React from 'react'
+   import React, { useContext, useEffect, useRef } from "react";
+   import { AuthContext } from "../context/AuthContext";
+   import { ChatContext } from "../context/ChatContext";
 
-   const Message = () => {
+   const Message = ({ message }) => {
+   const { currentUser } = useContext(AuthContext);
+   const { data } = useContext(ChatContext);
+
+   const ref = useRef();
+
+   useEffect(() => {
+      ref.current?.scrollIntoView({ behavior: "smooth" });
+   }, [message]);
+
    return (
-      <div className='message owner'>
+      <div
+         ref={ref}
+         className={`message ${message.senderId === currentUser.uid && "owner"}`}
+      >
          <div className="messageInfo">
-            <img src="https://images.pexels.com/photos/4759765/pexels-photo-4759765.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
-            <span>just now</span>
+         <img
+            src={
+               message.senderId === currentUser.uid
+               ? currentUser.photoURL
+               : data.user.photoURL
+            }
+            alt=""
+         />
+         <span>just now</span>
          </div>
          <div className="messageContent">
-            <p>Hello</p>
-            <img src="https://images.pexels.com/photos/4759765/pexels-photo-4759765.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+         <p>{message.text}</p>
+         {message.img && <img src={message.img} alt="" />}
          </div>
       </div>
-   )
-   }
+   );
+   };
 
-   export default Message
+   export default Message;
