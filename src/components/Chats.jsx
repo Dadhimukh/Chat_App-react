8a3c7d97ad2d214f5c917524301c0b6fe8dc612f
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-03-31 14:15:38 +0530

    updated

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { onSnapshot, doc } from "firebase/firestore";
 import { db } from "../firebase";
 import { AuthContext } from "../context/AuthContext";
