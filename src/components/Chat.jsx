2a3118641a768af9017c2faf9d377e2b08fe3391
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-04-01 22:50:01 +0530

    updated

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
+import { ChatContext } from "../context/ChatContext";
 import Messages from "./Messages";
 import Input from "./Input";
-import { ChatContext } from "../context/ChatContext";
 
 const Chat = () => {
   const { data } = useContext(ChatContext);
