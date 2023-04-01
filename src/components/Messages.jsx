3a62e3b7e02a2364be9fb5a8f0ddc0ae7ec5bb67
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-04-01 22:52:34 +0530

    updated

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,32 +1,32 @@
-   import React, { useContext, useEffect, useState } from "react";
-   import { doc, onSnapshot } from "firebase/firestore";
-   import { ChatContext } from "../context/ChatContext";
-   import { db } from "../firebase";
-   import Message from "./Message";
+import React, { useContext, useEffect, useState } from "react";
+import { doc, onSnapshot } from "firebase/firestore";
+import { ChatContext } from "../context/ChatContext";
+import { db } from "../firebase";
+import Message from "./Message";
 
-   const Messages = () => {
-   const [messages, setMessages] = useState([]);
-   const { data } = useContext(ChatContext);
+const Messages = () => {
+  const [messages, setMessages] = useState([]);
+  const { data } = useContext(ChatContext);
 
-   useEffect(() => {
-      const unSub = onSnapshot(doc(db, "chats", data.chatId), (doc) => {
-         doc.exists() && setMessages(doc.data().messages);
-      });
+  useEffect(() => {
+    const unSub = onSnapshot(doc(db, "chats", data.chatId), (doc) => {
+      doc.exists() && setMessages(doc.data().messages);
+    });
 
-      return () => {
-         unSub();
-      };
-   }, [data.chatId]);
+    return () => {
+      unSub();
+    };
+  }, [data.chatId]);
 
-   // console.log(messages);
+  // console.log(messages);
 
-   return (
-      <div className="messages">
-         {messages.map((m) => (
-         <Message message={m} key={m.id} />
-         ))}
-      </div>
-   );
-   };
+  return (
+    <div className="messages">
+      {messages.map((m) => (
+        <Message message={m} key={m.id} />
+      ))}
+    </div>
+  );
+};
 
-   export default Messages;
+export default Messages;
